ed03eeba6fe79995f8bc319209a7a5387ec525b7
Jayakumar <54398164+jay-kumar-jk@users.noreply.github.com>
2023-02-28 16:43:51 +0530

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import openai
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("API test")
+    print("Received event")
+    openai.api_key = "Enter your key"
+
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+    prompt = event['headers']['search']
+    
+    # Generate a response
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    response = completion.choices[0].text
+
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
